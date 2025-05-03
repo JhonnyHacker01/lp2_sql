eb244e33a166c0586c57b1bf6bcdde572f5ad468
JhonnyHacker01 <2021210353@udh.edu.pe>
2025-05-03 07:43:10 -0500

    prueba

--- code.sql
+++ code.sql
@@ -3,7 +3,7 @@ LABORTORIO 1
 ### **1. Mostrar los nombres y apellidos de los profesores de una facultad**
 
 ```sql
-SELECT nombres, apellidos
+SELECT nombres, apellido
 FROM usuario
 WHERE tipo = 'profesor';
 ```
